bdb353d455c041b360359cd454b80cb0b4ee9028
Brand28 <alejandrobrand28@gmail.com>
2021-11-07 20:07:54 -0500

    funcionalidad completa, trabajdno en ui

--- src/index.js
+++ src/index.js
@@ -1,27 +1,72 @@
 
 import{initializeApp} from "firebase/app";
-import{getDatabase, ref, set, onValue} from "firebase/database";
+import{getDatabase, ref, set, onValue, push} from "firebase/database";
 
 import {getFirebaseConfig} from "./firebase-config";
+import {studentLine} from "./studentLine";
 
 const firebaseAppConfig = getFirebaseConfig();
 const firebaseApp = initializeApp(firebaseAppConfig);
 
-
+// =================================================================================== //
 
 // Registrar Estudiantes
 function studentRegister (student){
     const db = getDatabase();
-    const dbRef = ref(db, 'students/' + student.name);
+    const dbRef = push(ref(db, 'students'));
 
+    student["id"] = dbRef.key;
     set(dbRef, student);
 }
+
+// Obtener Estudiantes
+function getStudents(){
+    const db = getDatabase();
+    const dbRef = ref(db, 'students');
+
+    onValue(dbRef, (snapshot) => {
+        const data = snapshot.val();
+        update(data);
+    });
+}
+
+// Actializar tablas con info nueva
+function update(data){
+    if(data){
+        noBonusContainer.innerHTML = "";
+        silverBonusContainer.innerHTML = "";
+        goldBonusContainer.innerHTML = "";
+
+        Object.keys(data).forEach((k, index) => {
+            console.log(k, index);
+            const student = new studentLine(data[k]);
+
+            // sort
+            if(data[k].score <= 5){
+                noBonusContainer.appendChild(student.render());
+            }else if(data[k].score > 5 && data[k].score <= 10){
+                silverBonusContainer.appendChild(student.render());
+            }else{
+                goldBonusContainer.appendChild(student.render());
+            }
+        });
+    } else{
+        noBonusContainer.innerHTML = "Empty";
+        silverBonusContainer.innerHTML = "Empty";
+        goldBonusContainer.innerHTML = "Empty";
+    }
+}
+
+// =================================================================================== //
 
 const name = document.getElementById("name");
 const code = document.getElementById("code");
 const course = document.getElementById("course");
 const bnEnroll = document.getElementById("bnEnroll");
-const studentsContainer = document.getElementById('studentsContainer');
+
+const noBonusContainer = document.getElementById('noBonusContainer');
+const silverBonusContainer = document.getElementById('silverBonusContainer');
+const goldBonusContainer = document.getElementById('goldBonusContainer');
 
 
 // Crear estudiente
@@ -36,29 +81,17 @@ const registerEvent = (e, event) =>{
         name: name.value,
         code: code.value,
         course: course.value,
+        score: 0,
     }
     studentRegister(student);
 
     name.value = '';
     code.value = '';
     course.value = '';
-    update();
 }
 
 // Click Botones
 bnEnroll.addEventListener('click', registerEvent);
+getStudents();
 
-function update(){
-    const db = getDatabase();
-    const dbRef = ref(db, 'students');
-
-    onValue(dbRef, function(data){
-        data.forEach(
-            student => {
-                let value = student.val();
-                console.log(value.name);
-                let line = new StudentLine(value);
-                studentsContainer.appendChild(line.render());
-            });
-    });
-}
+// =================================================================================== //
---
+++ src/studentLine.js
@@ -0,0 +1,68 @@
+import { getDatabase, ref, push, set } from 'firebase/database';
+
+export class studentLine{
+    constructor(student){
+        this.student = student;
+    }
+
+    render(){
+        // Componente Padre
+        let comp = document.createElement("div");
+        comp.className = "component";
+
+
+        // Nombre del estudiante
+        let name = document.createElement("p");
+        name.className = "studentName";
+        name.innerHTML = this.student.name;
+
+        // Código del estudiante
+        let code = document.createElement("p");
+        code.className = "studentCode";
+        code.innerHTML = this.student.code;
+
+        // Curso del estudiante
+        let course = document.createElement("p");
+        course.className = "studentCourse";
+        course.innerHTML = this.student.course;
+
+        // Puntaje del estudiante
+        let score = document.createElement('h5');
+        score.className = "studentScore";
+        score.innerHTML = this.student.score;
+
+        // Botón borrar
+        let bnDelete = document.createElement('button');
+        bnDelete.className = "deleteButton";
+        bnDelete.innerHTML = "X";
+
+        // Botón sumar
+        let bnAdd = document.createElement('button');
+        bnAdd.className = "addButton";
+        bnAdd.innerHTML = "+";
+
+        // Evento Eliminar Componente
+        bnDelete.addEventListener("click", (e, ev) => {
+            const db = getDatabase();
+            const dbRef = ref(db, 'students/' + this.student.id);
+            set(dbRef, null);
+        });
+
+        // Evento Sumar Puntaje
+        bnAdd.addEventListener("click", (e, ev) => {
+            const db = getDatabase();
+            const dbRef = ref(db, 'students/' + this.student.id + '/score');
+            set(dbRef, this.student.score + 1);
+        });
+
+        comp.appendChild(name);
+        comp.appendChild(code);
+        comp.appendChild(course);
+        comp.appendChild(score);
+
+        comp.appendChild(bnDelete);
+        comp.appendChild(bnAdd);
+
+        return comp;
+    }
+}
